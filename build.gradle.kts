5489f946eb09d21c6435aca26047e8d8b631c504
zang227 <159381Zeringue@chsbr.net>
2020-03-17 04:34:38 -0500

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,70 @@
+buildscript {
+    repositories {
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    `java-library`
+    checkstyle
+}
+
+apply<BootstrapPlugin>()
+apply<VersionPlugin>()
+
+subprojects {
+    group = "com.openosrs.externals"
+
+    project.extra["PluginProvider"] = "Zang"
+    project.extra["ProjectUrl"] = ""
+    project.extra["PluginLicense"] = "GNU General Public License v3.0"
+
+    repositories {
+        jcenter()
+        mavenLocal()
+        maven(url = "https://repo.runelite.net")
+        maven(url = "https://jitpack.io")
+    }
+
+    apply<JavaPlugin>()
+    apply(plugin = "java-library")
+    apply(plugin = "checkstyle")
+
+    checkstyle {
+        maxWarnings = 0
+        toolVersion = "8.25"
+        isShowViolations = true
+        isIgnoreFailures = false
+    }
+
+    configure<JavaPluginConvention> {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    tasks {
+        withType<JavaCompile> {
+            options.encoding = "UTF-8"
+        }
+
+        withType<Jar> {
+            doLast {
+                copy {
+                    from("./build/libs/")
+                    into("../release/")
+                }
+            }
+        }
+
+        withType<AbstractArchiveTask> {
+            isPreserveFileTimestamps = false
+            isReproducibleFileOrder = true
+            dirMode = 493
+            fileMode = 420
+        }
+
+        withType<Checkstyle> {
+            group = "verification"
+        }
+    }
+}

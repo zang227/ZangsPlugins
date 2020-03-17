3d53e5397e24679b35cb5e9c896a4e48fab2e037
zang227 <159381Zeringue@chsbr.net>
2020-03-17 04:17:28 -0500

    add files

---
+++ fightcave2/fightcaves2.gradle.kts
@@ -0,0 +1,30 @@
+version = "0.0.1"
+
+project.extra["PluginName"] = "Fight Caves 2"
+project.extra["PluginDescription"] = "Re-add old runelite Fight Caves Plugin"
+
+dependencies {
+    annotationProcessor(Libraries.lombok)
+    annotationProcessor(Libraries.pf4j)
+
+    compileOnly("com.openosrs:runelite-api:$rlVersion")
+    compileOnly("com.openosrs:runelite-client:$rlVersion")
+
+    compileOnly(Libraries.guice)
+    compileOnly(Libraries.lombok)
+    compileOnly(Libraries.pf4j)
+}
+
+tasks {
+    jar {
+        manifest {
+            attributes(mapOf(
+                    "Plugin-Version" to project.version,
+                    "Plugin-Id" to nameToId(project.extra["PluginName"] as String),
+                    "Plugin-Provider" to project.extra["PluginProvider"],
+                    "Plugin-Description" to project.extra["PluginDescription"],
+                    "Plugin-License" to project.extra["PluginLicense"]
+            ))
+        }
+    }
+}
